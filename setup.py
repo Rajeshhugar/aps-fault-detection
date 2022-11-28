22fb602091605995c48fbda4d9d0d3e802789fe7
rajeshXT <rajeshhugar220@gmail.com>
2022-11-28 14:03:19 +0530

    folders_created

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
 from setuptools import find_packages,setup
 
+from typing import List
 
-def get_requirements():
-    pass
+HYPEN_E_DOT='-e .'
+REQUIREMENT_FILES_NAME ='requirements.txt'
 
+
+def get_requirements()->List[str]:
+    with open(REQUIREMENT_FILES_NAME) as requirement_files:
+        requirement_list= requirement_files.readlines()
+    requirement_list=[requirement_name.replace("/n","") for requirement_name in requirement_list]
+
+    if HYPEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPEN_E_DOT)
+    return requirement_list
 
 
 setup(
     name="sensor",
     version="0.0.1",
     author="ineuron",
-    author_email="hugarrajesh@gmail.com"
-    packages=find_packages()
-    install_requires =get_requirements(),
+    author_email="hugarrajesh@gmail.com",
+    packages=find_packages(),
+    install_requires =get_requirements()
 )
+
